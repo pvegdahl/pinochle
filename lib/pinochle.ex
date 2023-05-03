e6b14f8996ffd5f17761f1d7296f3340ca564567
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-03 09:23:27 -0400

    mix new

---
+++ lib/pinochle.ex
@@ -0,0 +1,18 @@
+defmodule Pinochle do
+  @moduledoc """
+  Documentation for `Pinochle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pinochle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pinochle_test.exs
@@ -0,0 +1,8 @@
+defmodule PinochleTest do
+  use ExUnit.Case
+  doctest Pinochle
+
+  test "greets the world" do
+    assert Pinochle.hello() == :world
+  end
+end
