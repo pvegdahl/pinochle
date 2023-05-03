2af22d4feaea12447d54a9b16dfaa0af7dc64c09
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-03 19:31:49 -0400

    Update mix formatter settings and rerun

--- lib/card.ex
+++ lib/card.ex
@@ -21,13 +21,9 @@ defmodule Pinochle.Card do
           boolean()
   def wins?(first, second, trump \\ nil)
 
-  def wins?(
-        %Pinochle.Card{rank: first_rank, suit: suit},
-        %Pinochle.Card{rank: second_rank, suit: suit},
-        _trump
-      ) do
-    rank_index_0 = rank_index(first_rank)
-    rank_index_1 = rank_index(second_rank)
+  def wins?(%Pinochle.Card{rank: rank_0, suit: suit}, %Pinochle.Card{rank: rank_1, suit: suit}, _trump) do
+    rank_index_0 = rank_index(rank_0)
+    rank_index_1 = rank_index(rank_1)
     rank_index_0 >= rank_index_1
   end
 
