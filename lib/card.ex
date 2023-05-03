e5b0d19423c3f8055238a14146a4096fb6bba7aa
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-03 10:02:33 -0400

    Add clear typing for suits and ranks

--- lib/card.ex
+++ lib/card.ex
@@ -2,8 +2,12 @@ defmodule Pinochle.Card do
   @enforce_keys [:rank, :suit]
   defstruct [:rank, :suit]
 
-  @spec new(rank :: atom(), suit :: atom()) :: Pinochle.Card.t()
+  @type suit() :: :diamonds | :clubs | :hearts | :spades
+
+  @type rank() :: :nine | :jack | :queen | :king | :ten | :ace
+
+  @spec new(rank :: rank(), suit :: suit()) :: Pinochle.Card.t()
   def new(rank, suit), do: %Pinochle.Card{rank: rank, suit: suit}
 
-  @type t :: %__MODULE__{rank: atom(), suit: atom()}
+  @type t :: %__MODULE__{rank: rank(), suit: suit()}
 end
