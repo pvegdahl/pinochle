260d7f1bf136bbcfd09aa155934e85ad38cc6938
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-03 09:58:53 -0400

    Install dialyzer and use it to fix struct typing

--- lib/card.ex
+++ lib/card.ex
@@ -2,7 +2,8 @@ defmodule Pinochle.Card do
   @enforce_keys [:rank, :suit]
   defstruct [:rank, :suit]
 
-  @spec new(rank :: atom(), suit :: atom()) :: Pinochle.Card
+  @spec new(rank :: atom(), suit :: atom()) :: Pinochle.Card.t()
   def new(rank, suit), do: %Pinochle.Card{rank: rank, suit: suit}
 
+  @type t :: %__MODULE__{rank: atom(), suit: atom()}
 end
