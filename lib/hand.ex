332f68cfbe2d26bf36845ca7095fcaab79799bf4
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-06 11:37:56 -0400

    Add some Hand typing
    In addition, fix Hand.t() to be a possibly empty list.  At the
    end of each game, every hand will be empty, so my typing should
    probably support that. :-)

--- lib/hand.ex
+++ lib/hand.ex
@@ -1,5 +1,5 @@
 defmodule Pinochle.Hand do
-  @type t :: [Pinochle.Card, ...]
+  @type t :: [Pinochle.Card]
 
   @spec deal() :: [Pinochle.Hand.t(), ...]
   def deal() do
@@ -8,10 +8,12 @@ defmodule Pinochle.Hand do
     |> Enum.chunk_every(12)
   end
 
+  @spec remove(hand :: Pinochle.Hand.t(), card :: Pinochle.Card.t()) :: Pinochle.Hand.t()
   def remove(hand, card) do
     hand |> List.delete(card)
   end
 
+  @spec remove_multiple(hand :: Pinochle.Hand.t(), card :: [Pinochle.Card.t()]) :: Pinochle.Hand.t()
   def remove_multiple(hand, cards) do
     Enum.reduce(cards, hand, &List.delete(&2, &1))
   end
