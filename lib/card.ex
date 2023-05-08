d1b6f160180bf73ac517eabb012e308400ca5033
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-07 21:47:10 -0400

    More alias usage cleanup

--- lib/card.ex
+++ lib/card.ex
@@ -1,4 +1,6 @@
 defmodule Pinochle.Card do
+  alias Pinochle.Card, as: Card
+
   @enforce_keys [:rank, :suit]
   defstruct [:rank, :suit]
 
@@ -6,8 +8,8 @@ defmodule Pinochle.Card do
 
   @type rank() :: :nine | :jack | :queen | :king | :ten | :ace
 
-  @spec new(rank :: rank(), suit :: suit()) :: Pinochle.Card.t()
-  def new(rank, suit), do: %Pinochle.Card{rank: rank, suit: suit}
+  @spec new(rank :: rank(), suit :: suit()) :: Card.t()
+  def new(rank, suit), do: %Card{rank: rank, suit: suit}
 
   @type t :: %__MODULE__{rank: rank(), suit: suit()}
 
@@ -17,23 +19,23 @@ defmodule Pinochle.Card do
   @spec suits() :: [suit()]
   def suits(), do: [:diamonds, :clubs, :hearts, :spades]
 
-  @spec first_wins?(first :: Pinochle.Card.t(), second :: Pinochle.Card.t(), trump :: suit()) :: boolean()
+  @spec first_wins?(first :: Card.t(), second :: Card.t(), trump :: suit()) :: boolean()
   def first_wins?(first, second, trump)
 
-  def first_wins?(%Pinochle.Card{rank: rank_0, suit: suit}, %Pinochle.Card{rank: rank_1, suit: suit}, _trump) do
+  def first_wins?(%Card{rank: rank_0, suit: suit}, %Card{rank: rank_1, suit: suit}, _trump) do
     rank_index(rank_0) >= rank_index(rank_1)
   end
 
-  def first_wins?(_first, %Pinochle.Card{suit: trump}, trump), do: false
+  def first_wins?(_first, %Card{suit: trump}, trump), do: false
   def first_wins?(_first, _second, _trump), do: true
 
-  @spec second_wins?(first :: Pinochle.Card.t(), second :: Pinochle.Card.t(), trump :: suit()) :: boolean()
+  @spec second_wins?(first :: Card.t(), second :: Card.t(), trump :: suit()) :: boolean()
   def second_wins?(first, second, trump), do: !first_wins?(first, second, trump)
 
   @spec rank_index(rank :: rank()) :: integer
   def rank_index(rank), do: Enum.find_index(ranks(), fn r -> r == rank end)
 
-  @spec deck() :: [Pinochle.Card.t(), ...]
+  @spec deck() :: [Card.t(), ...]
   def deck() do
     for(rank <- ranks(), suit <- suits(), do: new(rank, suit))
     |> Enum.flat_map(&[&1, &1])
--- test/card_test.exs
+++ test/card_test.exs
@@ -1,46 +1,46 @@
 defmodule CardTest do
   use ExUnit.Case
 
-  import Pinochle.Card
+  alias Pinochle.Card, as: Card
 
   test "Create a card" do
-    assert new(:queen, :spades) == %Pinochle.Card{rank: :queen, suit: :spades}
+    assert Card.new(:queen, :spades) == %Card{rank: :queen, suit: :spades}
   end
 
   test "Get all ranks" do
-    assert ranks() == [:nine, :jack, :queen, :king, :ten, :ace]
+    assert Card.ranks() == [:nine, :jack, :queen, :king, :ten, :ace]
   end
 
   test "Get all suits" do
-    assert suits() == [:diamonds, :clubs, :hearts, :spades]
+    assert Card.suits() == [:diamonds, :clubs, :hearts, :spades]
   end
 
   test "First card wins if they are the same" do
-    assert first_wins?(new(:ace, :clubs), new(:ace, :clubs), :spades)
+    assert Card.first_wins?(Card.new(:ace, :clubs), Card.new(:ace, :clubs), :spades)
   end
 
   test "First card loses if it is lower in the same suit" do
-    assert !first_wins?(new(:king, :clubs), new(:ace, :clubs), :diamonds)
+    assert !Card.first_wins?(Card.new(:king, :clubs), Card.new(:ace, :clubs), :diamonds)
   end
 
   test "First card wins if it is higher in the same suit" do
-    assert first_wins?(new(:king, :clubs), new(:jack, :clubs), :clubs)
+    assert Card.first_wins?(Card.new(:king, :clubs), Card.new(:jack, :clubs), :clubs)
   end
 
   test "First card wins if suits do not match" do
-    assert first_wins?(new(:nine, :diamonds), new(:ten, :clubs), :hearts)
+    assert Card.first_wins?(Card.new(:nine, :diamonds), Card.new(:ten, :clubs), :hearts)
   end
 
   test "An off suit trump card wins" do
-    assert !first_wins?(new(:ace, :spades), new(:nine, :hearts), :hearts)
+    assert !Card.first_wins?(Card.new(:ace, :spades), Card.new(:nine, :hearts), :hearts)
   end
 
   test "Deck has 48 cards" do
-    assert Enum.count(deck()) == 48
+    assert Enum.count(Card.deck()) == 48
   end
 
   test "Deck has 2 each of 24 distinct cards" do
-    card_frequencies = deck() |> Enum.frequencies()
+    card_frequencies = Card.deck() |> Enum.frequencies()
     assert card_frequencies |> Enum.count() == 24
     assert card_frequencies |> Map.values() |> Enum.uniq() == [2]
   end
