dd17e6a2391978f1b423e52550d7614c420e204b
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-07 21:21:24 -0400

    Add type specs

--- lib/trick.ex
+++ lib/trick.ex
@@ -7,11 +7,13 @@ defmodule Pinochle.Trick do
   @spec new(starting_player :: 0..3) :: Pinochle.Trick.t()
   def new(starting_player), do: %Pinochle.Trick{starting_player: starting_player, cards: []}
 
+  @spec current_player(trick :: Pinochle.Trick.t()) :: 0..3
   def current_player(%Pinochle.Trick{starting_player: starting_player, cards: cards}) do
     (starting_player + Enum.count(cards))
     |> Integer.mod(4)
   end
 
+  @spec play_card(trick :: Pinochle.Trick.t(), card :: Pinochle.Card.t()) :: Pinochle.Trick.t()
   def play_card(%Pinochle.Trick{cards: cards} = trick, card) do
     %Pinochle.Trick{trick | cards: [card | cards]}
   end
