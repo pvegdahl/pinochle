7570eb35772ea03928803874d908a40c16f68b86
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-07 21:20:09 -0400

    Add current_player() and play_card() for Tricks

--- lib/trick.ex
+++ lib/trick.ex
@@ -1,6 +1,18 @@
 defmodule Pinochle.Trick do
-  @type t :: [Pinochle.Card.t()]
 
-  @spec new() :: Pinochle.Trick.t()
-  def new(), do: []
+  defstruct [:starting_player, :cards]
+
+  @type t :: %__MODULE__{starting_player: 0..3, cards: [Pinochle.Card.t()]}
+
+  @spec new(starting_player :: 0..3) :: Pinochle.Trick.t()
+  def new(starting_player), do: %Pinochle.Trick{starting_player: starting_player, cards: []}
+
+  def current_player(%Pinochle.Trick{starting_player: starting_player, cards: cards}) do
+    (starting_player + Enum.count(cards))
+    |> Integer.mod(4)
+  end
+
+  def play_card(%Pinochle.Trick{cards: cards} = trick, card) do
+    %Pinochle.Trick{trick | cards: [card | cards]}
+  end
 end
--- test/trick_test.exs
+++ test/trick_test.exs
@@ -4,6 +4,27 @@ defmodule TrickTest do
   import Pinochle.Trick
 
   test "New tricks are empty" do
-    assert new() |> Enum.count() == 0
+    assert new(0) |> Map.get(:cards) |> Enum.count() == 0
+  end
+
+  test "The current player for a new trick is the starting player" do
+    0..3 |> Enum.each(fn n -> assert new(n) |> current_player() == n end)
+  end
+
+  test "Playing a card increments the current_player" do
+    assert new(0) |> play_n_cards(1) |> current_player() == 1
+    assert new(1) |> play_n_cards(2) |> current_player() == 3
+  end
+
+  defp play_n_cards(trick, 0), do: trick
+  defp play_n_cards(trick, n) do
+    trick
+    |> play_card(Pinochle.Card.new(:jack, :diamonds))
+    |> play_n_cards(n-1)
+  end
+
+  test "The current_player count wraps from 3 -> 0" do
+    assert new(3) |> play_n_cards(1) |> current_player() == 0
+    assert new(2) |> play_n_cards(3) |> current_player() == 1
   end
 end
