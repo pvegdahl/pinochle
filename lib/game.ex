43dcefa873ffa81e3fe4ee8143b53ba5c29e8c9a
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-15 10:54:00 -0400

    Added a failing test for Game.play_card

--- lib/game.ex
+++ lib/game.ex
@@ -1,14 +1,21 @@
 defmodule Pinochle.Game do
   alias Pinochle.Game, as: Game
+  alias Pinochle.Hand, as: Hand
 
-  @enforce_keys [:current_player]
-  defstruct [:current_player]
+  @enforce_keys [:current_player, :hands]
+  defstruct [:current_player, :hands]
 
-  @type t :: %__MODULE__{current_player: 0..3}
+  @type t :: %__MODULE__{current_player: 0..3, hands: [Hand.t()]}
 
   @spec new(starting_player :: 0..3) :: Game.t()
-  def new(starting_player), do: %Game{current_player: starting_player}
+  def new(starting_player), do: %Game{current_player: starting_player, hands: Hand.deal()}
 
   @spec current_player(game :: Game.t()) :: 0..3
   def current_player(%Game{current_player: current_player}), do: current_player
+
+  def hand(%Game{hands: hands}, player) do
+    Enum.at(hands, player)
+  end
+
+  def play_card(game, card), do: nil
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -2,8 +2,30 @@ defmodule GameTest do
   use ExUnit.Case
 
   alias Pinochle.Game, as: Game
+  alias Pinochle.Card, as: Card
 
-  test "New game has current player" do
+  test "A new game has current player" do
     0..3 |> Enum.each(fn n -> assert Game.new(n) |> Game.current_player() == n end)
+  end
+
+  test "A new game has a 12 card hand for each player" do
+    game = Game.new(0)
+    0..3 |> Enum.each(fn n -> assert Game.hand(game, n) |> Enum.count() == 12 end)
+  end
+
+  test "A player playing a card increments to the next player" do
+    game = Game.new(0) |> Game.play_card(Card.new(:queen, :clubs))
+
+    assert Game.current_player(game) == 1
+  end
+
+  defp sorted_game(starting_player) do
+    # Each player will have all cards of one suit: clubs, diamonds, hearts, spades
+    hands =
+      Card.deck()
+      |> Enum.sort_by(fn card -> card.suit end)
+      |> Enum.chunk_every(12)
+
+    %Game{current_player: starting_player, hands: hands}
   end
 end
