3e174a72810c66ce420462e8f2f0e1a876586e8d
Philip Vegdahl <pvegdahl@gmail.com>
2023-05-19 15:20:22 +0000

    Add type specs

--- lib/game.ex
+++ lib/game.ex
@@ -1,5 +1,6 @@
 defmodule Pinochle.Game do
   alias Pinochle.Game, as: Game
+  alias Pinochle.Card, as: Card
   alias Pinochle.Hand, as: Hand
 
   @enforce_keys [:current_player, :hands]
@@ -13,13 +14,15 @@ defmodule Pinochle.Game do
   @spec current_player(game :: Game.t()) :: 0..3
   def current_player(%Game{current_player: current_player}), do: current_player
 
+  @spec hand(game :: Game.t(), player :: 0..3) :: Hand.t()
   def hand(%Game{hands: hands}, player) do
     Enum.at(hands, player)
   end
 
+  @spec play_card(game :: Game.t(), card :: Card.t()) :: Game.t()
   def play_card(%Game{current_player: current_player, hands: hands} = game, card) do
     new_current_player = Integer.mod(current_player + 1, 4)
-    new_hand = hand(game, current_player) |> Hand.remove_card(card)
+    new_hand = current_hand(game) |> Hand.remove_card(card)
     new_hands = List.replace_at(hands, current_player, new_hand)
 
     %Game{game | current_player: new_current_player, hands: new_hands}
