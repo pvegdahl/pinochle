d16630eed758d6a19e8cbf4319b4681678348542
Philip Vegdahl <pvegdahl@gmail.com>
2023-06-11 17:11:58 -0400

    Include the game_state in PlayerView

--- lib/player_view.ex
+++ lib/player_view.ex
@@ -1,13 +1,13 @@
 defmodule Pinochle.PlayerView do
   alias Pinochle.{PlayerView, Game, Hand, TrickTaking}
 
-  @enforce_keys [:hand]
-  defstruct [:hand]
+  @enforce_keys [:game_state, :hand]
+  defstruct [:game_state, :hand]
 
-  @type t :: %__MODULE__{hand: Hand.t()}
+  @type t :: %__MODULE__{game_state: Game.game_state(), hand: Hand.t()}
 
   @spec from_game(game :: Game.t(), player :: 0..3) :: PlayerView.t()
-  def from_game(%Game{data: %TrickTaking{} = trick_taking} = game, player) do
-    %PlayerView{hand: TrickTaking.hand(trick_taking, player)}
+  def from_game(%Game{game_state: :trick_taking, data: %TrickTaking{} = trick_taking} = game, player) do
+    %PlayerView{game_state: :trick_taking, hand: TrickTaking.hand(trick_taking, player)}
   end
 end
--- test/player_view_test.exs
+++ test/player_view_test.exs
@@ -12,4 +12,14 @@ defmodule PlayerViewTest do
 
     assert player_view.hand == TrickTaking.hand(trick_taking, player)
   end
+
+  test "PlayerView has the correct game state" do
+    trick_taking = TrickTaking.new(2, :hearts)
+    game = %Game{game_state: :trick_taking, data: trick_taking}
+    player = 1
+
+    player_view = PlayerView.from_game(game, player)
+
+    assert player_view.game_state == :trick_taking
+  end
 end
