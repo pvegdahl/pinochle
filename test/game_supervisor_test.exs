fe3291ba01b9da821bbdb100c72ef3ae00cfd196
Philip Vegdahl <pvegdahl@gmail.com>
2023-08-22 21:01:39 -0700

    Do cleanup of all processes on exit

--- test/game_supervisor_test.exs
+++ test/game_supervisor_test.exs
@@ -3,6 +3,16 @@ defmodule GameSupervisorTest do
 
   alias Pinochle.GameSupervisor
 
+  setup do
+    on_exit(&stop_all_games/0)
+  end
+
+  defp stop_all_games() do
+    DynamicSupervisor.which_children(GameSupervisor)
+    |> Enum.map(fn {_, pid, _, _} -> pid end)
+    |> Enum.each(&DynamicSupervisor.terminate_child(GameSupervisor, &1))
+  end
+
   test "Start two games in the GameSupervisor" do
     GameSupervisor.start_game("Bill's Game")
     GameSupervisor.start_game("Michael's Game")
