e28fb187089331d68fb86ec6b7a9dc1dd990c9a8
Philip Vegdahl <pvegdahl@gmail.com>
2023-08-22 21:42:18 -0700

    [REFACTOR] pull out common logic into suits of rank
    I don't love this name.  "Fortunately" I don't think this
    function will stick around as I complete the meld
    functionality.

--- lib/meld.ex
+++ lib/meld.ex
@@ -6,21 +6,19 @@ defmodule Pinochle.Meld do
   end
 
   defp marriages(hand, _trump) do
-    queen_suits =
-      hand
-      |> Enum.filter(fn card -> card.rank == :queen end)
-      |> Enum.map(fn card -> card.suit end)
-      |> Enum.into(MapSet.new())
-
-    king_suits =
-      hand
-      |> Enum.filter(fn card -> card.rank == :king end)
-      |> Enum.map(fn card -> card.suit end)
-      |> Enum.into(MapSet.new())
+    queen_suits = suits_of_rank(hand, :queen)
+    king_suits = suits_of_rank(hand, :king)
 
     MapSet.intersection(queen_suits, king_suits)
     |> MapSet.size()
     |> Kernel.*(2)
+  end
+
+  defp suits_of_rank(hand, rank) do
+    hand
+    |> Enum.filter(fn card -> card.rank == rank end)
+    |> Enum.map(fn card -> card.suit end)
+    |> Enum.into(MapSet.new())
   end
 
   defp nines(hand, trump) do
