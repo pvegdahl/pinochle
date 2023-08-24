b6b03706bf9dee6cff787288e96a5ac79033f45c
Philip Vegdahl <pvegdahl@gmail.com>
2023-08-24 08:15:07 -0700

    [REFACTOR] Split up counting and scoring in different functions

--- lib/meld.ex
+++ lib/meld.ex
@@ -3,10 +3,12 @@ defmodule Pinochle.Meld do
 
   @spec score(hand :: Hand, trump :: Card.suit()) :: non_neg_integer()
   def score(hand, trump) do
-    nines(hand, trump) + marriages_non_trump(hand, trump) + marriages_of_trump(hand, trump)
+    score_nines(hand, trump) + score_marriages_non_trump(hand, trump) + score_marriages_of_trump(hand, trump)
   end
 
-  defp marriages_non_trump(hand, trump) do
+  defp score_marriages_non_trump(hand, trump), do: 2 * count_marriages_non_trump(hand, trump)
+
+  defp count_marriages_non_trump(hand, trump) do
     hand_without_trump = Enum.reject(hand, fn card -> card.suit == trump end)
     queen_suits = count_suits_of_rank(hand_without_trump, :queen)
     king_suits = count_suits_of_rank(hand_without_trump, :king)
@@ -14,17 +16,18 @@ defmodule Pinochle.Meld do
     min_of_two_maps(queen_suits, king_suits)
     |> Map.values()
     |> Enum.sum()
-    |> Kernel.*(2)
   end
 
-  defp marriages_of_trump(hand, trump) do
+  defp score_marriages_of_trump(hand, trump), do: 4 * count_marriages_of_trump(hand, trump)
+
+  defp count_marriages_of_trump(hand, trump) do
     queen_of_trump = Card.new(:queen, trump)
     king_of_trump = Card.new(:king, trump)
 
     queens = Enum.count(hand, fn card -> card == queen_of_trump end)
     kings = Enum.count(hand, fn card -> card == king_of_trump end)
 
-    min(queens, kings) * 4
+    min(queens, kings)
   end
 
   defp count_suits_of_rank(hand, rank) do
@@ -37,7 +40,7 @@ defmodule Pinochle.Meld do
     Map.intersect(map1, map2, fn _k, v1, v2 -> min(v1, v2) end)
   end
 
-  defp nines(hand, trump) do
+  defp score_nines(hand, trump) do
     Enum.count(hand, fn card -> card == Card.new(:nine, trump) end)
   end
 end
